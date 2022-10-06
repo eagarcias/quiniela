097259ad46e991216109785d199af1b5f8396749
Erick Garcia <alejandro.erick2310@gmail.com>
2022-10-06 11:56:54 -0600

    fix: change redirection after update and create

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -35,7 +35,7 @@ class MatchesController < ApplicationController
         respond_to do |format|
             
             if @match.save
-                format.html { redirect_to match_url(@match), notice: "Match was successfully created." }
+                format.html { redirect_to matches_url, notice: "Match was successfully created." }
                 format.json { render :show, status: :created, location: @match }
             else
                 format.html { render :new, status: :unprocessable_entity }
@@ -57,7 +57,7 @@ class MatchesController < ApplicationController
         end 
         respond_to do |format|
             if @match.update(match_params)
-                format.html { redirect_to match_url(@match), notice: "Match was successfully updated." }
+                format.html { redirect_to matches_url, notice: "Match was successfully updated." }
                 format.json { render :show, status: :ok, location: @match }
             else
                 format.html { render :edit, status: :unprocessable_entity }
